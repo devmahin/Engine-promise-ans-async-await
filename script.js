9c3f66ac16d5b86be507ee5624b9d4552f038969
devmahin <mahinhowlader123@gmail.com>
2024-02-29 07:46:31 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// function name (){
+//     console.log("My name is mahin howlader")
+// }
+// function stu (){
+//     console.log("1 am a student.")
+// }
+// function live (){
+//     console.log("1 live in barishal.")
+// }
+
+//  function loop (){
+//   setTimeout(() =>{
+//     let  classs = 0;
+//     while(classs < 10000){
+//        console.log(classs)
+//        classs++;
+//    }
+//   }, 2000)
+// }
+
+// name()
+// stu()
+// loop()
+// live()
